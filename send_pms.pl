47c1a6435f0f11f769ef04a0e02d115729efddb8
Fwiffo <borgified@gmail.com>
2012-09-18 16:08:35 -0700

    working on gui

---
+++ send_pms.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/env perl
+
+use CGI qw/:standard/;
+
+print header,start_html;
+
+print <<HTML;
+<h1>Recall inactive pilots back into active duty</h1>
+<form action="pm_mech.pl" method="POST">
+<input type="checkbox" name="inactive" value="grey"> grey inactives <br>
+<input type="checkbox" name="inactive" value="red"> red inactives <br>
+forum username <input type="text" name="username"><br>
+forum password <input type="text" name="password"><br>
+<textarea cols="80" rows="10" name="message"></textarea><br>
+<input type="submit" value="Submit">
+</form>
+</html>
+HTML
+;
+
+
