e372435df72c4f1c09bfeb2488b4fee1515905a8
Fwiffo <borgified@gmail.com>
2012-09-20 13:52:26 -0700

    workin version

--- pm_mech.pl
+++ pm_mech.pl
@@ -5,20 +5,21 @@ use strict;
 
 use WWW::Mechanize;
 use LWP::Debug;
-
+use CGI ':standard';
 
 my %config = do "/secret/alleg.config";
 
-
+my $username=param('username');
+my $password=param('password');
 
 my $agent = WWW::Mechanize->new();
 
 $agent->get('http://www.freeallegiance.org/forums/index.php');
 $agent->follow_link(text => 'Log In', n => '1');
 $agent->form_name('LOGIN');
-$agent->field('UserName', $config{'username'});
+$agent->field('UserName', $username);
 $agent->tick('CookieDate', '1');
-$agent->field('PassWord', $config{'password'});
+$agent->field('PassWord', $password);
 $agent->click();
 
 $agent->get('http://www.freeallegiance.org/forums/index.php?act=Msg&amp;CODE=04');
@@ -33,5 +34,6 @@ $agent->field('msg_title', 'test');
 $agent->field('entered_name', 'fwiffo');
 $agent->click_button( number => 1);
 
-$agent->save_content('a.html');
+#$agent->save_content('a.html');
 
+print header;
