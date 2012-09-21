0443f192fb19e4cf7489b1d277c0470529e52764
borgified <borgified@gmail.com>
2012-09-20 18:18:27 -0700

    squad leadership identified

--- squad_roster.pl
+++ squad_roster.pl
@@ -17,10 +17,22 @@ shift(@squads);
 
 foreach my $squad (@squads){
 
-	$squad=~/^(.*?)&nbsp;/;
+	$squad=~/^(.*?)&nbsp;.*\(\@(.*?)\)/;
 	my $squad_name=$1;
+	my $tag=$2;
 	print "--------------------------\n";
-	print $squad_name,"\n";
+	print $squad_name," ",$tag,"\n";
+
+
+#identify squad leadership
+	my @squad_leadership;
+	my @pilots=split(/<br>/i,$squad);
+	foreach my $pilot (@pilots){
+		if($pilot =~ /[\*\+\^](.*?) /){
+			push(@squad_leadership,$1);
+		}
+	}
+	print "squad leadership: @squad_leadership\n";
 
 #inactives (>30 days)
 	my @inactives = split(/<strike>/,$squad);
