dd39df899565eba19f2c1bd44e65776cf7f23acf
borgified <borgified@gmail.com>
2012-09-30 12:16:21 -0700

    now only accepts members of squad leadership to use this app

--- pm_mech.pl
+++ pm_mech.pl
@@ -15,11 +15,30 @@ my %input;
 
 $input{'username'}=param('username');
 $input{'password'}=param('password');
+$input{'squad'}=param('squad');
+$input{'messaage'}=param('messaage');
 
+print header;
+
+#check if $username belongs to squad leadership, if not, exit with a message
+my $leaders = Squadroster::list_leadership($input{'squad'});
+
+my $authorized=0;
+
+foreach my $pilot (@$leaders){
+	if($input{'username'} =~ /\b$pilot\b/i){
+		$authorized=1;
+	}
+}
+
+unless($authorized){
+	print "you must be part of your squad's leadership to use this app.<br>";
+	print "these are your leaders: @$leaders\n";
+	exit;
+}
 
 #PM::send_pm(\%input);
 
-print header;
 print "grey: ",param('grey'),"\n";
 print "red: ",param('red'),"\n";
 print "radio: ",param('squad'),"\n";
